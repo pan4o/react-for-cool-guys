ed0bd80dbbcca50c81fbc2874880b4c1bf7ce000
Pancho <panchenko923@gmail.com>
2018-01-03 23:38:19 +0200

    first cool commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <p>Hello Serhii</p>
+        );
+    }
+}
+
+export default App;
