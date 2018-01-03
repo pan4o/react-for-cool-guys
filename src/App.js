6e384a8c5a9a7bb82240d6c652a3741f6036276d
Pancho <panchenko923@gmail.com>
2018-01-04 00:40:42 +0200

    3.1.1.2 Использование записи JSX через точку

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return (
-            <p>Hello Serhii</p>
+            <div>
+                <p>Hello Serhii</p>
+                <SuccessButton text="OK" />
+            </div>
         );
     }
+}
+
+// Доступ к компоненту через точку
+
+const MyComponents = {
+    Button: function(props) {
+        return <button>{props.text}</button>;
+    }
+};
+
+function SuccessButton(props) {
+    return <MyComponents.Button text={props.text} />;
 }
 
 export default App;
